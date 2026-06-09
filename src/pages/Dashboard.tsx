018e184523b6564ed2c581cbe907a89c358f13a8
Kasia Jankowska <katarzyna.jankowska000728@gmail.com>
2026-06-09 10:56:37 +0200

    Add antd

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,10 @@
+import { Button } from "antd";
+
 export const DashboardPage = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
+    <div className="flex flex-col gap-4 h-screen w-screen items-center justify-center">
       <h1 className="text-3xl font-bold">Dashboard</h1>
+      <Button onClick={() => alert("Hello")}>Click me</Button>
     </div>
   );
 };
